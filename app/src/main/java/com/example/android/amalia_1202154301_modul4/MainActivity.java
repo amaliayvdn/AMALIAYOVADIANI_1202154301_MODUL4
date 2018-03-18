719b6878a4294e6e3265c92b00baf48868541321
amaliayvdn <36080338+amaliayvdn@users.noreply.github.com>
2018-03-18 15:16:26 +0700

    Initial commit

---
+++ app/src/main/java/com/example/android/amalia_1202154301_modul4/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.android.amalia_1202154301_modul4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button LM, IMG; Intent move;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LM = (Button)findViewById(R.id.listNamaBtn);
+        IMG = (Button)findViewById(R.id.GambarBtn);
+
+        LM.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                move = new Intent(MainActivity.this, ListMahasiswa.class);
+                startActivity(move);
+            }
+        });
+
+        IMG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                move = new Intent(MainActivity.this, PencariGambar.class);
+                startActivity(move);
+            }
+        });
+
+    }
+}
+
